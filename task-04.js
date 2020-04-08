719253e995a9dbae0c1a339e84b61624cb757b38
Griffin <chemist_khnu@rambler.ru>
2020-04-09 01:16:35 +0300

    JS HW 1

---
+++ task-04.js
@@ -0,0 +1,15 @@
+const pricePerDroid = 3000;
+let credits = 23580;
+let totalPrice;
+const numberOfDroids = prompt('Сколько дроидов вы хотите купить?');
+if (numberOfDroids === null) {
+  console.log('Отменено пользователем!');
+} else {
+  totalPrice = pricePerDroid * numberOfDroids;
+  if (totalPrice > credits) {
+    console.log('Недостаточно средств на счету!');
+  } else {
+    credits = credits - totalPrice;
+    console.log('Вы купили', numberOfDroids, 'дроидов, на счету осталось', credits, 'кредитов.');
+  }
+}
