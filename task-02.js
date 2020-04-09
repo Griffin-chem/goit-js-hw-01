f70ca9f2fc2394ec435f672d104a1ebefecfc926
Griffin <chemist_khnu@rambler.ru>
2020-04-09 21:27:51 +0300

    JS HW1 Ready

--- task-02.js
+++ task-02.js
@@ -1,6 +1,7 @@
 "use strict";
 const total = 100;
 const ordered = 80;
+// (ordered > total) ? console.log('На складе недостаточно товаров!') : console.log('Заказ оформлен, с вами свяжется менеджер');
 if (ordered > total) {
   console.log('На складе недостаточно товаров!')
 } else {
